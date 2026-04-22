975784886670420927ee72649e09f0a413e73d4c
Philipp Förderer <p.foerderer@gmx.de>
2026-04-22 23:05:58 +0200

    Renaming

---
+++ Testpprojekt/TestprojektMain.cs
@@ -0,0 +1,19 @@
+﻿using Tools.Console;
+using System.Reflection;
+
+namespace Testpprojekt
+{
+    internal class TestprojektMain
+    {
+
+        public static int Main(string[] args)
+        {
+            ConsoleTools.tl("Programmstart ( " + DateTime.Now.ToString("HH:mm:ss") + ")" + Assembly.GetExecutingAssembly().GetName().FullName + "...");
+
+           
+
+            ConsoleTools.tl("...Programmende!");
+            return 0;
+        }
+    }
+}
---
+++ Tools/ConsoleTools.cs
@@ -0,0 +1,22 @@
+﻿namespace Tools
+{
+    namespace Console
+    {
+        public class ConsoleTools
+        {
+            public static void tl(string message)
+            {
+                System.Console.WriteLine(message);
+            }
+            public static void t(string message)
+            {
+                System.Console.Write(message);
+            }
+
+            public static void tl(object value)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}

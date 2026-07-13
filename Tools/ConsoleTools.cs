6d5c4e49d49a05477ce88cd53018c597f6b0fb2d
Philipp Förderer <p.foerderer@gmx.de>
2026-07-13 23:10:44 +0200

    Update für Tools und Solution

--- Tools/ConsoleTools.cs
+++ Tools/ConsoleTools.cs
@@ -19,4 +19,24 @@
             }
         }
     }
+    namespace Program {
+        public class ProgramTools {
+            public static void StartProgram(string [] args) {
+                if (args != null && args.Length > 0) {
+                    Console.ConsoleTools.tl($"Programm startet mit Parameter(n):");
+                    foreach (var arg in args) {
+                        Console.ConsoleTools.tl($"\t -  {arg}");
+                    }
+                } 
+                else {
+                    Console.ConsoleTools.tl($"\nProgramm startet ohne Parameter....\n");
+                }
+            }
+            public static int EndProgram(string arg) {
+                Console.ConsoleTools.tl($"\nProgrammende erreicht.\t (" + arg + ")\n(Taste drücken, zum Beenden...)");
+                System.Console.ReadKey();
+                return 0;
+            }
+        }
+    }
 }
